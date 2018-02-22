d9db7895c0b489ad87ae92b5801b51d63ed1cb63
AmberGold <amberxgold911@hotmail.com>
2018-02-21 18:32:16 -0800

    Added menu for main listview activity

--- app/src/main/java/edu/fullerton/ecs/cpsc466/PlanList.java
+++ app/src/main/java/edu/fullerton/ecs/cpsc466/PlanList.java
@@ -3,6 +3,8 @@ package edu.fullerton.ecs.cpsc466;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 
@@ -14,4 +16,23 @@ public class PlanList extends AppCompatActivity {
         setContentView(R.layout.activity_plan_list);
     }
 
+    //show options bar menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_list_view_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add_plan:
+                Intent intent = new Intent(this, PlanEvent.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
